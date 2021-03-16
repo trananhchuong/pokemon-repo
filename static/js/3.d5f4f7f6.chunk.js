1eda3a162a87db58490267737e4a5c5ac31fe74e
Chuong Tran <39957959+trananhchuong@users.noreply.github.com>
2021-03-16 21:36:19 +0700

    Updates

---
+++ static/js/3.d5f4f7f6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppokemon=this.webpackJsonppokemon||[]).push([[3],{367:function(n,e,t){"use strict";t.r(e);t(0);var o=t(3);e.default=function(){return Object(o.jsx)("div",{children:"Generations"})}}}]);
+//# sourceMappingURL=3.d5f4f7f6.chunk.js.map
